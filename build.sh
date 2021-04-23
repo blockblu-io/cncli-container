169db62840ca8e6b4b960a91c799bf68ef2e95ec
Kevin Haller <staking@outofbits.com>
2021-04-23 17:27:55 +0200

    Build scripts is now building images for multiple platforms, namely linux/amd64, linux/arm/v7 and linux/arm64/v8.

--- build.sh
+++ build.sh
@@ -5,4 +5,33 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-docker build --build-arg CNCLI_VERSION=$1 --no-cache --pull -t adalove/cncli:$1 .
+docker build --platform linux/amd64 \
+	--build-arg CNCLI_VERSION=$1 \
+	--no-cache \
+	--pull \
+	-t "adalove/cncli:$1_amd64" .
+
+docker push "adalove/cncli:$1_amd64"
+
+docker build --platform linux/arm/v7 \
+	--build-arg CNCLI_VERSION=$1 \
+	--no-cache \
+	--pull \
+	-t "adalove/cncli:$1_arm" .
+
+docker push "adalove/cncli:$1_arm"
+
+docker build --platform linux/arm64/v8 \
+	--build-arg CNCLI_VERSION=$1 \
+	--no-cache \
+	--pull \
+	-t "adalove/cncli:$1_arm64" .
+
+docker push "adalove/cncli:$1_arm64"
+
+docker manifest create "adalove/cncli:$1" \
+	"adalove/cncli:$1_amd64" \
+	"adalove/cncli:$1_arm" \
+	"adalove/cncli:$1_arm64"
+
+docker manifest push --purge "adalove/cncli:$1"
